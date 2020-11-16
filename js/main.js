76fdb37c94d7157f8cd85b0377466e243574a045
ATGuardian <anhtuan101294@gmail.com>
2020-11-16 11:07:03 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ $('.lazy-load').fadeOut(1500);
 $(document).ready(function (e) {
     $('.carousel-content1 h1').slideDown(1200);
     $('.carousel-content1 span').slideDown(1500);
-    // Locomotive JS
-    import LocomotiveScroll from './lib';
-
-    const scroll = new LocomotiveScroll();
+  
+   
     $('.main-carousel').flickity({
         // options
         cellAlign: 'left',
